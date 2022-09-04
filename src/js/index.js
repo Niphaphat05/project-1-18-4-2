1b60751b7b954cb640007c7a881d546d5adccdce
Niphaphat Nakpong <niphaphat.b@gmail.com>
2022-09-04 23:02:18 +0700

    ใส่VantaJS และ Footer

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+VANTA.CLOUDS({
+    el: "#page-header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    skyColor: 0x158c76,
+    cloudShadowColor: 0x61b9b3
+})
